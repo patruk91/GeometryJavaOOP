057cd157a4ad408bc6708f467219a3cf8f8fd85c
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:08:05 +0100

    Implement methods for display, calculate area, perimeter for rectangle

--- src/main/java/com/codecool/java/geometry/shapes/Rectangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Rectangle.java
@@ -1,4 +1,31 @@
 package com.codecool.java.geometry.shapes;
 
 public class Rectangle extends Shape {
+    private float a;
+    private float b;
+
+    public Rectangle(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double calculateArea() {
+        return a * b;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * a + 2 * b;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return "a×b";
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return "2×a+2×b";
+    }
 }
