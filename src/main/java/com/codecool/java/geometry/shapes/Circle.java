04e8518b08f378426f51f1b99d5ee1ffa1c11d4d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:03:24 +0100

    Implement methods to display, calculate area, perimeter for circle

--- src/main/java/com/codecool/java/geometry/shapes/Circle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Circle.java
@@ -1,4 +1,29 @@
 package com.codecool.java.geometry.shapes;
 
 public class Circle extends Shape {
+    private float r;
+
+    public Circle(float r) {
+        this.r = r;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(r, 2);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * r;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return "π×r^2";
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return "2×π×r";
+    }
 }
