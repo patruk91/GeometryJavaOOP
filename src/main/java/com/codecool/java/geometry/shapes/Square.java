790f73726f27b4c9abd9477993b7d82345788b3f
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:09:17 +0100

    Inherits methods from Rectangle class

--- src/main/java/com/codecool/java/geometry/shapes/Square.java
+++ src/main/java/com/codecool/java/geometry/shapes/Square.java
@@ -1,4 +1,10 @@
 package com.codecool.java.geometry.shapes;
 
 public class Square extends Rectangle {
+    private float a;
+
+    public Square(float a) {
+        super(a, a);
+        this.a = a;
+    }
 }
