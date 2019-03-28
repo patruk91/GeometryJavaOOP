fc3de9001042b17e1d66777cb1809a5d48024289
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:14:31 +0100

    Implement methods to add, get largest shape by permiter, area

--- src/main/java/com/codecool/java/geometry/containers/ShapeList.java
+++ src/main/java/com/codecool/java/geometry/containers/ShapeList.java
@@ -1,4 +1,46 @@
 package com.codecool.java.geometry.containers;
 
+import com.codecool.java.geometry.shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShapeList {
+    List<Shape> shapes;
+
+    public ShapeList() {
+        this.shapes = new ArrayList<>();
+    }
+
+    public void addShape(Shape shape) {
+        this.shapes.add(shape);
+    }
+
+    public Shape getShapeAt(int index) {
+        return shapes.get(index);
+    }
+
+    public String getShapesTable() {
+        return "make a table";
+    }
+
+    public Shape getLargestShapeByPerimeter() {
+        Shape biggestPerimeter = shapes.get(0);
+        for (Shape shape : this.shapes) {
+            if (shape.calculatePerimeter() > biggestPerimeter.calculatePerimeter()) {
+                biggestPerimeter = shape;
+            }
+        }
+        return biggestPerimeter;
+    }
+
+    public Shape getLargestShapeByArea() {
+        Shape biggestArea = shapes.get(0);
+        for (Shape shape : this.shapes) {
+            if (shape.calculateArea() > biggestArea.calculateArea()) {
+                biggestArea = shape;
+            }
+        }
+        return biggestArea;
+    }
 }
