a4b556d8265eb71df8d3667a3c6542601e8ab7c3
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:07:03 +0100

    Inherits methods from class Triangle

--- src/main/java/com/codecool/java/geometry/shapes/EquilateralTriangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/EquilateralTriangle.java
@@ -1,4 +1,11 @@
 package com.codecool.java.geometry.shapes;
 
 public class EquilateralTriangle extends Triangle {
+    private float a;
+
+    public EquilateralTriangle(float a) {
+        super(a, a, a);
+        this.a = a;
+    }
+
 }
