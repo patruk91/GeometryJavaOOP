69c055fc280ab83c004efcaf9f52dec13810029b
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 11:45:20 +0100

    Add first option to menu

--- src/main/java/com/codecool/java/geometry/Main.java
+++ src/main/java/com/codecool/java/geometry/Main.java
@@ -1,6 +1,7 @@
 package com.codecool.java.geometry;
 
 import com.codecool.java.geometry.containers.ShapeList;
+import com.codecool.java.ui.Handler;
 import com.codecool.java.ui.Reader;
 import com.codecool.java.ui.View;
 
@@ -8,7 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         View view = new View();
-        Reader reader = new Reader();
+        Reader reader = new Reader(view);
+        Handler handler = new Handler(view, reader);
 	    ShapeList shapes = new ShapeList();
 	    boolean isRunning = true;
 
@@ -20,6 +22,8 @@ public class Main {
             switch (option) {
                 case 1:
                     view.printShapes();
+                    int addOption = reader.getIntFromUser("Option", 1, 6);
+                    handler.handleAddShape(addOption, shapes);
                     break;
                 case 2:
                     // Show all shapes
