4ecdc24e3a08d84033466e32c036d4ec3c3c71ba
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:04:13 +0100

    Implement methods to display, calculate area, permiter for triangle

--- src/main/java/com/codecool/java/geometry/shapes/Triangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Triangle.java
@@ -1,4 +1,35 @@
 package com.codecool.java.geometry.shapes;
 
 public class Triangle extends Shape {
+    private float a;
+    private float b;
+    private float c;
+
+    public Triangle(float a, float b, float c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double calculateArea() {
+        float s = (a + b + c) / 2;
+        return Math.sqrt(s * (s -a) * (s - b) * (s - c));
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return a + b + c;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return "s = (a+b+c)/2\n" +
+                "sqrt(s×(s-a)×(s-b)×(s-c)";
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return "a+b+c";
+    }
 }
