928b690f89b1d9b67f72e8c1937aeeddc71f6508
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 10:33:36 +0100

    Add a view class and methods to print menu, message

---
+++ src/main/java/com/codecool/java/ui/Validator.java
@@ -0,0 +1,16 @@
+package com.codecool.java.ui;
+
+public class Validator {
+
+    public boolean validateString(String input) {
+        return !input.isBlank();
+    }
+
+    public boolean isNumeric(String input) {
+        return input.matches("[\\d]+");
+    }
+
+    public boolean isNumberInRange(int start, int end, int input) {
+        return (start <= input && input <= end);
+    }
+}
