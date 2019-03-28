1d45babf64f213b24f88955c43b86e11dd2b4485
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-28 09:11:19 +0100

    Implement methods for display, calculate area, perimiter is regular pentagon

--- src/main/java/com/codecool/java/geometry/shapes/RegularPentagon.java
+++ src/main/java/com/codecool/java/geometry/shapes/RegularPentagon.java
@@ -1,4 +1,29 @@
 package com.codecool.java.geometry.shapes;
 
 public class RegularPentagon extends Shape {
+    private float a;
+
+    public RegularPentagon(float a) {
+        this.a = a;
+    }
+
+    @Override
+    public double calculateArea() {
+        return (Math.pow(a, 2)* Math.sqrt(5 * (5 + 2 * Math.sqrt(5)))) / 4;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 5 * a;
+    }
+
+    @Override
+    public String getAreaFormula() {
+        return "(a^2×sqrt(5×(5+2×sqrt(5))))/4";
+    }
+
+    @Override
+    public String getPerimeterFormula() {
+        return "5×a";
+    }
 }
